bef0560ae854c5d0316e8a1a65f6c7cb3cb15583
Imtiaz <itsimtiaz@outlook.com>
2024-08-12 18:38:56 +0500

    Add 'RemoveFirst', 'RemoveLast', 'IndexOf', 'Contains', 'Reverse', and 'FindFindKthNodeFromEnd' functionalities to custom linklist implementation

--- DataStructures-Algorithms-CSharp/DataStructures/Linklist/CustomLinkList.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/Linklist/CustomLinkList.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace DataStructures_Algorithms_CSharp.DataStructures.Linklist;
 
 public class CustomLinkList
@@ -26,6 +24,8 @@ public class CustomLinkList
             node.Next = Head;
             Head = node;
         }
+
+        offset++;
     }
 
     public void AddLast(int item)
@@ -41,7 +41,129 @@ public class CustomLinkList
             Tail!.Next = node;
             Tail = node;
         }
+
+        offset++;
     }
 
+    public void RemoveFirst()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("The list is empty.");
+        }
+
+        if (Head == Tail)
+        {
+            Head = Tail = null;
+        }
+        else
+        {
+            var nextNode = Head!.Next;
+            Head!.Next = null;
+            Head = nextNode;
+        }
+
+        offset--;
+    }
+
+    public void RemoveLast()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("The list is empty.");
+        }
+        else
+        {
+            var current = Head;
+
+            while (current!.Next != Tail)
+            {
+                current = current!.Next;
+            }
+
+            current.Next = null;
+            Tail = current;
+        }
+
+        offset--;
+    }
+
+    public int IndexOf(int item)
+    {
+        var current = Head;
+
+        int j = 0;
+        while (current != null)
+        {
+            if (current.Item == item)
+            {
+                return j;
+            }
+
+            j++;
+        }
+        return -1;
+    }
+
+    public bool Contains(int item) => IndexOf(item) >= 0;
+
+    public int Size() => offset;
+
+    public void Reverse()
+    {
+        if (IsEmpty() || Size() == 1)
+        {
+            return;
+        }
+
+        Node? current = Head, next = null, previous = null;
+
+        while (current != null)
+        {
+            next = current.Next;
+
+            current.Next = previous;
+
+            previous = current;
+            current = next;
+        }
+
+        // Resetting Head and Tail
+        Tail = Head;
+        Head = previous;
+    }
+
+    public int KthNodeFromEnd(int index)
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("This list is empty.");
+        }
+
+        var current = Head;
+        for (int i = 0; i < index; i++)
+        {
+            current = current!.Next;
+
+            if (current is null)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+
+        var kthNode = Head;
+        while (current != null)
+        {
+            current = current.Next;
+            kthNode = kthNode!.Next;
+        }
+
+        return kthNode!.Item;
+    }
+
+    #region Methods
+
     private bool IsEmpty() => Head is null;
+
+    #endregion
 }
