fabf5698d72be37b7ba7c634304f10eff04f60ec
Imtiaz <itsimtiaz@outlook.com>
2024-08-15 17:09:18 +0500

    Add TwoStacksInArray Class. It is the implementation of two stacks within one array.

---
+++ DataStructures-Algorithms-CSharp/DataStructures/Stack/TwoStacksInArray.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DataStructures_Algorithms_CSharp.DataStructures.Stack;
+
+public class TwoStacksInArray
+{
+    private int[] stack;
+    private int stack1Counter, stack2Counter;
+
+    public TwoStacksInArray(int size)
+    {
+        stack = new int[size];
+    }
+}
