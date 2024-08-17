3edc8667f681c1d121f669d69b80dbc1a8c49c77
Imtiaz <itsimtiaz@outlook.com>
2024-08-17 20:19:08 +0500

    Add Reverse method that can be used to reverse element up-to k inside queue

--- DataStructures-Algorithms-CSharp/DataStructures/Queue/QueueReverser.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/Queue/QueueReverser.cs
@@ -24,4 +24,35 @@ public class QueueReverser
             queue.Enqueue(stack.Pop());
         }
     }
+
+    /// <summary>
+    /// It will reverse elements up-to k element.
+    /// </summary>
+    /// <param name="queue"></param>
+    /// <param name="k"></param>
+    public void Reverse(CustomQueue queue, int k)
+    {
+
+        if (queue.IsEmpty())
+        {
+            throw new InvalidOperationException("The queue is empty.");
+        }
+
+        CustomStack stack = new CustomStack(queue.Count());
+
+        for (int i = 0; i < k; i++)
+        {
+            stack.Push(queue.Dequeue());
+        }
+
+        while (!stack.IsEmpty())
+        {
+            queue.Enqueue(stack.Pop());
+        }
+
+        for (int i = 0; i < queue.Count() - k; i++)
+        {
+            queue.Enqueue(queue.Dequeue());
+        }
+    }
 }
