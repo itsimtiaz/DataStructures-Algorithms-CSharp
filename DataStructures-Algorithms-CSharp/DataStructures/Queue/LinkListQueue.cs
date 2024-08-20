d627dc7379e5a1827c049ddb006ec2cf7cb286d7
Imtiaz <itsimtiaz@outlook.com>
2024-08-20 19:43:39 +0500

    Add Dequeue, Peek, IsEmpty, Size, and ToArray methods.

--- DataStructures-Algorithms-CSharp/DataStructures/Queue/LinkListQueue.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/Queue/LinkListQueue.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 namespace DataStructures_Algorithms_CSharp.DataStructures.Queue;
 
 public class LinkListQueue
@@ -38,7 +40,46 @@ public class LinkListQueue
         {
             throw new InvalidOperationException("The queue is empty.");
         }
+
+        var element = Head;
+        Head = Head!.Next;
+
+        element!.Next = null;
+        _count--;
+        return element.Item;
+    }
+
+    public int Peek()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("The queue is empty.");
+        }
+
+        return Head!.Item;
     }
 
     public bool IsEmpty() => Head == null;
+
+    public int Size() => _count;
+
+    public int[] ToArray()
+    {
+
+        var list = new int[_count];
+
+        if (!IsEmpty())
+        {
+            int i = 0;
+            var current = Head;
+
+            while (current != null)
+            {
+                list[i++] = current.Item;
+            }
+
+        }
+
+        return list;
+    }
 }
