be1297e9cca1b6aa3ec508193006311f4c727442
Imtiaz <itsimtiaz@outlook.com>
2024-08-21 20:13:58 +0500

    Add Peek and IsEmpty methods in TwoStacksQueue class.

--- DataStructures-Algorithms-CSharp/DataStructures/Queue/TwoStacksQueue.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/Queue/TwoStacksQueue.cs
@@ -22,6 +22,29 @@ public class TwoStacksQueue
             throw new InvalidOperationException("The queue is empty.");
         }
 
+        MoveStack1ToStack2();
+
+        return stack2.Pop();
+    }
+
+    public int Peek()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("The queue is empty.");
+        }
+
+        MoveStack1ToStack2();
+
+        return stack2.Peek();
+    }
+
+    public bool IsEmpty() => !stack1.Any() && !stack2.Any();
+
+    #region Methods
+
+    private void MoveStack1ToStack2()
+    {
         if (!stack2.Any())
         {
             while (stack1.Any())
@@ -29,10 +52,8 @@ public class TwoStacksQueue
                 stack2.Push(stack1.Pop());
             }
         }
-
-        return stack2.Pop();
     }
 
-    public bool IsEmpty() => !stack1.Any() && !stack2.Any();
+    #endregion
 
 }
