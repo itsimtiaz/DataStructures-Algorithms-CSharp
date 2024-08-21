e3402aca8d886324f121370e7b6d11cb36ba162e
Imtiaz <itsimtiaz@outlook.com>
2024-08-21 20:07:10 +0500

    Add Enqueue and Dequeue methods in TwoStacksQueue class.

--- DataStructures-Algorithms-CSharp/DataStructures/Queue/TwoStacksQueue.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/Queue/TwoStacksQueue.cs
@@ -2,5 +2,37 @@ namespace DataStructures_Algorithms_CSharp.DataStructures.Queue;
 
 public class TwoStacksQueue
 {
+    private Stack<int> stack1, stack2;
+
+    public TwoStacksQueue()
+    {
+        stack1 = new Stack<int>();
+        stack2 = new Stack<int>();
+    }
+
+    public void Enqueue(int item)
+    {
+        stack1.Push(item);
+    }
+
+    public int Dequeue()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("The queue is empty.");
+        }
+
+        if (!stack2.Any())
+        {
+            while (stack1.Any())
+            {
+                stack2.Push(stack1.Pop());
+            }
+        }
+
+        return stack2.Pop();
+    }
+
+    public bool IsEmpty() => !stack1.Any() && !stack2.Any();
 
 }
