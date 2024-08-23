64f41e2f9989c01824002063ec29e1bd46d49076
Imtiaz <itsimtiaz@outlook.com>
2024-08-23 23:36:52 +0500

    Add 'Add' method.

--- DataStructures-Algorithms-CSharp/DataStructures/HashTable/CustomHashTable.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/HashTable/CustomHashTable.cs
@@ -11,10 +11,43 @@ public class CustomHashTable
             Key = key;
             Value = value;
         }
-        public int Key { get; private set; }
-        public string Value { get; private set; } = null!;
+        public int Key { get; set; }
+        public string Value { get; set; } = null!;
     }
 
+    private LinkedList<TableKeyValuePair>[] _table;
 
+    public CustomHashTable(int size)
+    {
+        _table = new LinkedList<TableKeyValuePair>[size];
+    }
+
+    public void Add(int key, string value)
+    {
+        var tableIndex = GetTableHashCode(key);
+
+        if (_table[tableIndex] is null)
+        {
+            _table[tableIndex] = new LinkedList<TableKeyValuePair>();
+        }
+
+        // If item is already exist then we just need to update the value. 
+        foreach (var item in _table[tableIndex])
+        {
+            if (item.Key == key)
+            {
+                item.Value = value;
+                return;
+            }
+        }
+
+        _table[tableIndex].AddLast(new TableKeyValuePair(key, value));
+    }
+
+    #region Methods
+
+    private int GetTableHashCode(int key) => key.GetHashCode() % _table.Length;
+
+    #endregion
 
 }
