038d2888d242dd10e0fa2c1132160602e38a75cf
Imtiaz <itsimtiaz@outlook.com>
2024-09-05 16:18:20 +0500

    Add Add(item) method in CustomBinaryTree.

--- DataStructures-Algorithms-CSharp/DataStructures/Trees/CustomBinaryTree.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/Trees/CustomBinaryTree.cs
@@ -2,5 +2,55 @@ namespace DataStructures_Algorithms_CSharp.DataStructures.Trees;
 
 public class CustomBinaryTree
 {
+    class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+        public Node? Left { get; set; }
+        public Node? Right { get; set; }
+    }
+
+    private Node? _root;
+
+    public void Add(int item)
+    {
+        Node node = new(item);
+
+        if (_root is null)
+        {
+            _root = node;
+            return;
+        }
+
+        var currentNode = _root;
+
+        while (true)
+        {
+            if (item > currentNode.Value)
+            {
+                if (currentNode.Right is null)
+                {
+                    currentNode.Right = node;
+                    break;
+                }
+
+                currentNode = currentNode.Right;
+            }
+            else
+            {
+                if (currentNode.Left is null)
+                {
+                    currentNode.Left = node;
+                    break;
+                }
+                currentNode = currentNode.Left;
+            }
+        }
+
+    }
 
 }
