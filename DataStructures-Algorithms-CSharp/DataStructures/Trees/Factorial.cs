65551ccec35ae0007ff66c09b3f549a70484e022
Imtiaz <itsimtiaz@outlook.com>
2024-09-07 16:54:04 +0500

    Add Factorial class

---
+++ DataStructures-Algorithms-CSharp/DataStructures/Trees/Factorial.cs
@@ -0,0 +1,12 @@
+namespace DataStructures_Algorithms_CSharp.DataStructures.Trees;
+
+public class Factorial
+{
+    public int GetFactorial(int value)
+    {
+        if (value == 0)
+        {
+            return 1;
+        }
+    }
+}
