bd190faf5305565e224e1d0ea757b6c027c645a2
Imtiaz <itsimtiaz@outlook.com>
2024-09-07 16:56:52 +0500

    Add 'GetFactorialByRecursion' method.

--- DataStructures-Algorithms-CSharp/DataStructures/Trees/Factorial.cs
+++ DataStructures-Algorithms-CSharp/DataStructures/Trees/Factorial.cs
@@ -2,11 +2,13 @@ namespace DataStructures_Algorithms_CSharp.DataStructures.Trees;
 
 public class Factorial
 {
-    public int GetFactorial(int value)
+    public int GetFactorialByRecursion(int value)
     {
         if (value == 0)
         {
             return 1;
         }
+
+        return value * (value - 1);
     }
 }
