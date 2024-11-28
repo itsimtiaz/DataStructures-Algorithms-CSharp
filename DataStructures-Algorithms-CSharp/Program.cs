baf0e3cee42dafdba9da820e355bb9c3e29a65d0
itsimtiaz <itsimtiaz@outlook.com>
2024-11-28 19:25:26 +0500

    Added Remove method in CustomHeap class.

--- DataStructures-Algorithms-CSharp/Program.cs
+++ DataStructures-Algorithms-CSharp/Program.cs
@@ -1,1 +1,9 @@
-Console.WriteLine("Hello, World!");
+using DataStructures_Algorithms_CSharp.DataStructures.Heap;
+
+CustomHeap customHeap = new CustomHeap(10);
+
+customHeap.Add(1);
+customHeap.Add(2);
+customHeap.Add(3);
+
+Console.Read();
