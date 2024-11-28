20949906f86f62505b1c9fb6b3543aef0baa5308
itsimtiaz <itsimtiaz@outlook.com>
2024-11-28 18:27:51 +0500

    Add CustomHeap class.

---
+++ DataStructures-Algorithms-CSharp/DataStructures/Trees/CustomHeap.cs
@@ -0,0 +1,11 @@
+namespace DataStructures_Algorithms_CSharp.DataStructures.Trees;
+
+public class CustomHeap
+{
+    private readonly int[] _heap;
+
+    public CustomHeap(int size)
+    {
+        _heap = new int[size];
+    }
+}
